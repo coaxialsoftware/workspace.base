1534b6544c2dba062440dfc04766247b561f5473
Giancarlo Bellido <giancarlo.bellido@gmail.com>
2016-07-12 12:04:54 -0400

    Removed fixmyjs

--- workspace.jshint/index.js
+++ workspace.jshint/index.js
@@ -1,33 +1,32 @@
 
 var
 	jshint = require('jshint').JSHINT,
-	fixmyjs = require('fixmyjs'),
 	path = require('path'),
 	fs = require('fs'),
-	
+
 	common = workspace.common,
 	plugin = module.exports = cxl('workspace.jshint')
 ;
 
 plugin.extend({
-	
+
 	sourcePath: __dirname + '/jshint.js',
-	
+
 	findOptions: function(p, f)
 	{
 		var file = path.dirname(f) + '/.jshintrc', data;
-		
+
 		if (!fs.existsSync(file))
 		{
 			file = p + '/.jshintrc';
 			if (!fs.existsSync(file))
 				file = false;
 		}
-		
+
 		if (file)
 		{
 			this.dbg(`Using ${file} as config.`);
-			
+
 			try { data = JSON.parse(fs.readFileSync(file, 'utf8')); }
 			catch(e) {
 				this.dbg('Coult not read jshintrc file.');
@@ -37,7 +36,7 @@ plugin.extend({
 			}
 		}
 	},
-	
+
 	doLint: function(client, options, data, js)
 	{
 		jshint(js, options, options && options.globals);
@@ -48,10 +47,10 @@ plugin.extend({
 		payload.e = data.editor;
 
 		workspace.socket.respond(client, 'jshint', payload);
-		
+
 		return payload;
 	},
-	
+
 	lintFile: function(client, data)
 	{
 	var
@@ -60,16 +59,16 @@ plugin.extend({
 		common.readFile(data.f)
 			.then(this.doLint.bind(this, client, options, data));
 	},
-	
+
 	fixFile: function(client, data)
 	{
-	var
+	/*var
 		op = this.findOptions(data.p, data.f) || {},
 		fix = fixmyjs.fix(data.js, op)
 	;
-		console.log(fix);
+		console.log(fix);*/
 	},
-	
+
 	/**
 	 * data: { $:id, p:project, f:path, js:code }
 	 */
@@ -80,20 +79,20 @@ plugin.extend({
 		else if (data.op==='fix')
 			this.operation(`Fixing file ${data.f}`, this.fixFile.bind(this, client, data));
 	},
-	
+
 	onAssist: function(done, data)
 	{
 		if (!(data.fileChanged &&
 			(data.mime ==='application/json' || data.mime==='application/javascript')))
 			return;
-		
+
 		var options = this.findOptions(data.project, data.file);
 		this.doLint(data.client, options, data, data.content);
 	}
-	
+
 }).run(function() {
-	
+
 	workspace.plugins.on('assist', this.onAssist.bind(this));
 	workspace.plugins.on('socket.message.jshint', this.onMessage.bind(this));
-	
+
 });
