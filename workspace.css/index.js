39a69d4fa21a75f6bea204bc67e22acbe0a866fa
Giancarlo Bellido <giancarlo@coaxialhost.com>
2017-05-09 03:04:36 +0000

    Added CSS plugin

---
+++ workspace.css/index.js
@@ -0,0 +1,83 @@
+
+var
+	plugin = module.exports = cxl('workspace.css'),
+	// TODO
+	DEFS = require('./completions.json')
+;
+
+class CSSLanguageServer extends workspace.LanguageServer {
+
+	constructor()
+	{
+		super('css', /text\/(?:css|less|sass)/);
+	}
+
+	tag(match)
+	{
+		match.icon = 'tag';
+		return match;
+	}
+
+	property(match)
+	{
+		match.icon = 'property';
+		return match;
+	}
+
+	propertyLong(match, prop)
+	{
+		match.icon = 'property';
+		match.description = prop.description;
+		match.code = 'css';
+		return match;
+	}
+
+	onAssist(done, data)
+	{
+		var token = data.token, match;
+
+		if (!token)
+			return;
+
+		switch (token.type) {
+		case 'property error':
+		case 'property':
+			match = DEFS.properties[token.value];
+
+			if (match)
+				done([ {
+					title: token.value, icon: 'property', code: 'css',
+					description: match.description }
+				]);
+			break;
+		}
+	}
+
+	onInlineAssist(done, data)
+	{
+		var token = data.token;
+
+		switch (token.type) {
+		case 'tag':
+			return done(this.findArray(DEFS.tags, token.cursorValue, this.tag));
+		case 'property error':
+		case 'property':
+			return done(this.findObject(DEFS.properties, token.cursorValue, this.property));
+		// TODO pseudo-selector token type.
+		}
+	}
+
+}
+
+plugin.extend({
+
+	destroy: function()
+	{
+		this.ls.destroy();
+	}
+
+}).run(function() {
+
+	this.ls = new CSSLanguageServer();
+
+});
